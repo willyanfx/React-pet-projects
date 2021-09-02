f9e071c7784c99ab838309d85c142ecba7960f3c
willyanfx <willyan.fx@gmail.com>
2021-09-01 21:01:18 -0700

    first

---
+++ next-md-blog/pages/_app.js
@@ -0,0 +1,13 @@
+import Header from '../components/header'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return (<>
+    <Header />
+    <main className="container">
+      <Component {...pageProps} />
+    </main>
+  </>)
+}
+
+export default MyApp
---
+++ next-md-blog/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Dev blog</title>
+      </Head>
+
+      <main >
+       <h2>hello world!</h2>
+      </main>
+
+      <footer >
+     
+      </footer>
+    </div>
+  )
+}
